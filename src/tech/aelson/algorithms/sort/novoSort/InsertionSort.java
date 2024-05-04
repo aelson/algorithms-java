a25205c55ba4db44adf5f17f7551d7898d59a7b4
aelson <aelson.alves@gmail.com>
2024-05-04 00:38:14 -0230

    The Insertion Sort Algorithm

---
+++ src/tech/aelson/algorithms/sort/novoSort/InsertionSort.java
@@ -0,0 +1,17 @@
+package tech.aelson.algorithms.sort.novoSort;
+
+import tech.aelson.algorithms.model.Product;
+import tech.aelson.algorithms.util.Util;
+
+public class InsertionSort {
+    public static void execute(Product[] products, int numberOfElements) {
+        for (int current = 1; current < numberOfElements; current++) {
+            System.out.println("I am in the element " + current);
+            int elementBeingAnalysed = current;
+            while (elementBeingAnalysed > 0 && products[elementBeingAnalysed].getPrice() < products[elementBeingAnalysed - 1].getPrice()) {
+                Util.swap(products, elementBeingAnalysed, elementBeingAnalysed - 1);
+                elementBeingAnalysed--;
+            }
+        }
+    }
+}
