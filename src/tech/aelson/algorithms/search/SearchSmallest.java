b78eb83778fc40fb0b1760f36ad435daec7e9612
aelson <aelson.alves@gmail.com>
2024-05-03 23:11:33 -0230

    Rename tester class and executor method

--- src/tech/aelson/algorithms/search/SearchSmallest.java
+++ src/tech/aelson/algorithms/search/SearchSmallest.java
@@ -3,7 +3,7 @@ package tech.aelson.algorithms.search;
 import tech.aelson.algorithms.model.Product;
 
 public class SearchSmallest {
-    public static int findSmallest(Product[] products, int start, int end) {
+    public static int execute(Product[] products, int start, int end) {
         int smallest = start;
         for (int atual = start; atual <= end; atual++) {
             if (products[atual].getPrice() < products[smallest].getPrice()) {
--- src/tech/aelson/algorithms/search/SearchSmallestTester.java
+++ src/tech/aelson/algorithms/search/SearchSmallestTester.java
@@ -2,7 +2,7 @@ package tech.aelson.algorithms.search;
 
 import tech.aelson.algorithms.model.Product;
 
-public class TestSmallestPrice {
+public class SearchSmallestTester {
     public static void main(String[] args) {
         Product[] products = {
                 new Product("Tesla Model 3", 50000),
@@ -12,7 +12,7 @@ public class TestSmallestPrice {
                 new Product("Audi Q5", 45000)
         };
 
-        int smallest = SearchSmallest.findSmallest(products, 0, 4);
+        int smallest = SearchSmallest.execute(products, 0, 4);
         System.out.println(smallest);
         System.out.println("The car " + products[smallest].getName() + " costs " + products[smallest].getPrice());
     }
